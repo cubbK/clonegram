1f762e18623a535225678fddd57745989b2272e1
Dan <danpopa1998@gmail.com>
2018-03-28 14:38:43 +0300

    add footer

---
+++ src/components/FooterMain.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="footer md-primary">
+      Footer Info
+   </div>
+</template>
+
+<script>
+export default {
+  name: "FooterMain"
+};
+</script>
+
+<style>
+.footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70px;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,18 +2,20 @@
   <div>
     <header-main></header-main>
     <photo-cards></photo-cards>
-    123
+    <footer-main></footer-main>
   </div>
 </template>
 
 <script>
 import HeaderMain from "./HeaderMain.vue";
 import PhotoCards from "./PhotoCards.vue";
+import FooterMain from "./FooterMain.vue";
 export default {
   name: "Home",
   components: {
     HeaderMain,
-    PhotoCards
+    PhotoCards,
+    FooterMain
   }
 };
 </script>
